f86996fde0244916dcc6cdfa56ba6f149e675737
Chinthanadr02 <chinthanadr2@gmail.com>
2025-02-23 13:13:57 +0530

    Add files via upload

--- __init__.py
+++ __init__.py
@@ -1,13 +1,3 @@
-from flask import Blueprint
+# utils/__init__.py
 
-# Initialize Blueprints
-patient_routes = Blueprint('patient_routes', __name__)
-doctor_routes = Blueprint('doctor_routes', __name__)
-appointment_routes = Blueprint('appointment_routes', __name__)
-vital_routes = Blueprint('vital_routes', __name__)
-
-# Import the route files to register the routes
-from .patient_routes import patient_routes
-from .doctor_routes import doctor_routes
-from .appointment_routes import appointment_routes
-from .vital_routes import vital_routes
+# You can initialize package-level variables or imports here if needed
